4777b6da4e3465c929cccd752d9e3c0adc0add22
vishal-singh123 <vishal_sh1994@yahoo.in>
2017-12-14 16:45:59 +0530

    Changed welcome view to laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">VISHAL 5</div>
+                <div class="title">LARAVEL 5</div>
             </div>
         </div>
     </body>
